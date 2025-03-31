0acc32120c0fb298062fb7d012db9b39392490eb
Nils Kehrein <nils.kehrein@bayer.com>
2025-03-31 10:21:43 +0200

    Verification vignette is now pre-knitted to reduce pckg build time
    The original RMarkdown vignette for verification purposes is now
    located under vignettes/orig/. If any updates occur, it must be
    manually knit to a Markdown file stored under the same name in
    vignettes/.

--- dev/release.R
+++ dev/release.R
@@ -4,7 +4,7 @@ unlink(c("src-i386", "src-x64"), recursive = TRUE)
 unlink(c("Meta","inst/doc", "inst/Meta"), recursive = TRUE)
 
 # run CHECK
-Sys.setenv(CI="TRUE")
+Sys.setenv(CI="TRUE") #skip long-running tests
 devtools::check(vignettes = FALSE)
 Sys.setenv(CI="")
 
@@ -12,10 +12,18 @@ Sys.setenv(CI="")
 devtools::document()
 devtools::document()
 devtools::install(upgrade="never", quick=TRUE)
-devtools::build_vignettes(quiet=FALSE, install=FALSE)
-if(!dir.exists("docs")) dir.create("docs")
-file.copy(list.files("doc", pattern="\\.html$", full.names=TRUE), "docs", recursive=TRUE)
-unlink("doc", recursive = TRUE)
+
+# test-build the vignettes
+#devtools::build_vignettes(quiet=FALSE, install=FALSE)
+#unlink("doc", recursive = TRUE)
+
+# pre-knit verification vignette, because it takes too long to do on the fly
+withr::with_dir("vignettes", knitr::knit("orig/lemna-verification.Rmd", output = "lemna-verification.Rmd"))
+
+# copy HTML versions of the vignettes for the Github pages
+rmarkdown::render("vignettes/lemna-introduction.Rmd", output_format="html_document", output_dir="docs")
+rmarkdown::render("vignettes/lemna-verification.Rmd", output_format="html_document", output_dir="docs")
+
 
 # build package files
 unlink(list.files("src", pattern="*.o", full.names = TRUE))
